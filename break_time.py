b8a30c86f4f94a9859a4aa196151c48ab5c48950
vishal-kit <vishal.kit@outlook.com>
2016-06-30 18:00:41 +0900

    changes

--- break_time.py
+++ break_time.py
@@ -3,5 +3,5 @@ import time
 
 for i in range(1,8,3):
     time.sleep(10)
-    webbrowser.open("https://www.youtube.com/watch?v=vAqLStH6E6s")
+    webbrowser.open("https://www.youtube.com/watch?v=inlc4qYRlU8")
 print "Done"
